3958b0e60db11b17c488c8b61755f2e8b3e89277
branrojs <branrojs@gmail.com>
2018-03-02 18:37:42 -0600

    update 03022018 validation styles, post creation with api

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import {  createPost} from '../actions';
 
 class PostsNew extends Component {
 	renderField(field) {
 		//with the ...field we say ok this is an object right here adn i want all the differents propperties 
 		//and this object to be communicated as props to the input tag
+		// ternary expresion (condition ? true : false)
+
+		const { meta: {touched, error } } = field;
+		const className = `form-group ${ touched && error ? 'has-danger' : '' }`;
+
 		return (
-			<div className="form-group">
+			<div className={className}>
 				<label>{field.label}</label>
 				<input className="form-control" type="text"
 					{...field.input}
 				/>
-				{field.meta.error}
+				<div className="text-help">
+					{touched ? error : ''}
+				</div>
 			</div>
 		);
 	}
 
 	onSubmit(values){
-		console.log(values);
+		this.props.createPost(values);
 	}
 
 	render() {
@@ -43,6 +53,7 @@ class PostsNew extends Component {
 					component={ this.renderField }
 				/>
 				<button type="submit" className="btn btn-primary">submit</button>
+				<Link to="/" className="btn btn-danger">Cancel</Link>
 			</form>
 		);
 	}
@@ -72,9 +83,11 @@ function validate(values) {
 }
 
 
-// string assigned to form : must be unique 
+// string assigned to form : must be unique // combine redux connect with redux form
 export default reduxForm({
 	validate,
 	form: 'PostsNewForm'
-})(PostsNew);
+})(
+	connect( null,{ createPost })(PostsNew)
+);
 
